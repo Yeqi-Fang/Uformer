28c8c731d7a62bcb5fd8e78bd5fd79b3981adb1e
fangyeqi-mechu <fangyeqi202106@163.com>
2025-03-23 22:38:58 +0800

    debug

--- script/train_sinogram.sh
+++ script/train_sinogram.sh
@@ -1,3 +1,3 @@
-python3 ./train/train_denoise.py --arch Uformer_B --batch_size 4 --gpu '0' \
+python3 ./train/train_sinogram.py --arch Uformer_B --batch_size 4 --gpu '0' \
     --train_ps 128 --root_dir /mnt/d/fyq/sinogram/2e9div_smooth --env _0706\
       --save_dir ./logs/ --dataset sidd --warmup
